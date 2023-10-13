44ffea4542a23db882b9776eca5b1fc0d365893c
PatoVerde <leandrodenos@gmail.com>
2023-10-13 19:19:43 -0300

    Update[Stack]: texts

--- src/components/sections/Stack.js
+++ src/components/sections/Stack.js
@@ -18,35 +18,35 @@ const Stack = () => {
           <div className="xl:flex xl:justify-start">
             <a href="#" class="block max-w-sm p-4 bg-white border rounded-lg shadow bg-white-800 border-gray-700 hover:bg-gray-700 xl:mb-0 mb-5 mx-2 xl:mx-0">
               <h5 class="block mb-2 text-2xl font-bold tracking-tight">Solidity</h5>
-              <p class="font-normal">Two years on profesional experience building smart-contracts</p>
+              <p class="font-normal">Two years of professional experience in smart contract development, leadership roles and providing expertise in smart contract solutions.</p>
             </a>
           </div>
           <div className="xl:flex xl:justify-center">
             <a href="#" class="block max-w-sm p-4 bg-white border rounded-lg shadow bg-white-800 border-gray-700 hover:bg-gray-700 xl:mb-0 mb-5 mx-2 xl:mx-0">
               <h5 class="mb-2 text-2xl font-bold tracking-tight">Foundry</h5>
-              <p class="font-normal">Foundy is the best envirioment to make smart contracts</p>
+              <p class="font-normal">Foundry excels as a top-tier platform for smart contract development, testing, and deployment.</p>
             </a>
           </div>
           <div className="xl:flex xl:justify-end">
             <a href="#" class="block max-w-sm p-4 bg-white border rounded-lg shadow bg-white-800 border-gray-700 hover:bg-gray-700 xl:mb-0 mb-5 mx-2 xl:mx-0">
               <h5 class="mb-2 text-2xl font-bold tracking-tight">Hardhat</h5>
-              <p class="font-normal">Here are thogy acquisitions of 2021 so fogical order.</p>
+              <p class="font-normal">Hardhat is a widely used development environment the creation, testing, and deployment of smart contracts.</p>
             </a>
           </div>
             <a href="#" class="block max-w-sm p-4 bg-white border rounded-lg shadow bg-white-800 border-gray-700 hover:bg-gray-700 xl:mb-0 mb-5 mx-2 xl:mx-0">
               <h5 class="mb-2 text-2xl font-bold tracking-tight  ">JavaScript</h5>
-              <p class="font-normal">JS for dApps, ether.js web3.js wagmi</p>
+              <p class="font-normal">JavaScript is my tool of choice for architecting dApps that communicate with smart contracts, thanks to ethers.js and web3.js.</p>
             </a>
           <div className="xl:flex xl:justify-center">          
             <a href="#" class="block max-w-sm p-4 bg-white border rounded-lg shadow bg-white-800 border-gray-700 hover:bg-gray-700 xl:mb-0 mb-5 mx-2 xl:mx-0">
               <h5 class="mb-2 text-2xl font-bold tracking-tight  ">Defi</h5>
-              <p class="font-normal">JS for dApps, ether.js web3.js wagmi ether.js web3.js wagmi ether.js web3.js wagmi</p>
+              <p class="font-normal">DeFi specialist with a strong grasp of leading projects, specializing in lending, swapping, staking, and blockchain economies.</p>
             </a>
           </div>
           <div className="xl:flex xl:justify-end">
             <a href="#" class="block max-w-sm p-4 bg-white border rounded-lg shadow bg-white-800 border-gray-700 hover:bg-gray-700 xl:mb-0 mb-5 mx-2 xl:mx-0">
               <h5 class="mb-2 text-2xl font-bold tracking-tight  ">Blockchain</h5>
-              <p class="font-normal">JS for dApps, ether.js web3.js wagmiJSether.js web3.js wagmi</p>
+              <p class="font-normal">Deep expertise in diverse blockchains, skilled in seamless integration of solutions, and a trusted technical consultant.</p>
             </a>
           </div>
         </div>
